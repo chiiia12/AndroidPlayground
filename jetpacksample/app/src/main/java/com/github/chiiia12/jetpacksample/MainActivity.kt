cf2610b5d13ce7af2af2ceba96f1142fd97400ad
chiiia12 <llnsbchsep12@gmail.com>
2018-10-21 15:51:17 +0900

    add detail view

--- jetpacksample/app/src/main/java/com/github/chiiia12/jetpacksample/MainActivity.kt
+++ jetpacksample/app/src/main/java/com/github/chiiia12/jetpacksample/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.github.chiiia12.jetpacksample
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.github.chiiia12.jetpacksample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val viewModel = TodoViewModel()
+        binding.viewModel = viewModel
+        binding.setLifecycleOwner(this)
+        binding.doneButton.setOnClickListener {
+            viewModel.done()
+        }
     }
 }
