533031bab8f586ea8a35b8bd2a41fe62210188c4
chiiia12 <chiakiyokoo@gmail.com>
2019-06-21 18:25:02 +0900

    challenge mergeDelayError

--- KotlinCoroutinePlayground/app/src/main/java/com/example/kotlincoroutineplayground/MainActivity.kt
+++ KotlinCoroutinePlayground/app/src/main/java/com/example/kotlincoroutineplayground/MainActivity.kt
@@ -14,27 +14,36 @@ class MainActivity : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
         Timber.d("coroutine start")
         uiScope.launch {
-            runCatching {
-                val completeFirst = async { completeFirst() }
-                val completeSecond = async { completeSecond() }
-                Timber.d("==================")
-                Timber.d("${completeFirst.await()} ${completeSecond.await()}")
-            }.onFailure {
-                Timber.e(it)
+            val completeFirst = async { runCatching { completeFirst() } }
+            val completeSecond = async { runCatching { completeSecond() } }
+            Timber.d("==================")
+            Timber.d("${completeFirst.await()} ${completeSecond.await()}")
+            listOf(completeFirst.await(), completeSecond.await()).filter {
+                it.isFailure
+            }.let {
+                if (it.isEmpty()) {
+                    Timber.d("all job success")
+                } else {
+                    it.map { result ->
+                        Timber.d("failed ${result.exceptionOrNull()}")
+                    }
+                }
             }
+
         }
     }
 
     private suspend fun completeFirst(): String {
         delay(2000)
         Timber.d("completeFirst")
+        throw Throwable("test first")
         return "completeFirst"
     }
 
     private suspend fun completeSecond(): String {
         delay(1000)
         Timber.d("completeSecond")
-        throw Throwable("test throwable")
+        throw Throwable("test second")
         return "completeSecond"
     }
 }
