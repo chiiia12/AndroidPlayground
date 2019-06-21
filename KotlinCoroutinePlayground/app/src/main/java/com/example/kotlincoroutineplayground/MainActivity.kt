e2c1a69a1d7f6f4990e19c22e8d620ba2bafab3b
chiiia12 <chiakiyokoo@gmail.com>
2019-06-21 18:08:08 +0900

    revert init

--- KotlinCoroutinePlayground/app/src/main/java/com/example/kotlincoroutineplayground/MainActivity.kt
+++ KotlinCoroutinePlayground/app/src/main/java/com/example/kotlincoroutineplayground/MainActivity.kt
@@ -11,25 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Timber.plant(Timber.DebugTree())
-        Timber.d("coroutine start")
-        uiScope.launch {
-            val completeFirst = async { completeFirst() }.await()
-            val completeSecond = async { completeSecond() }.await()
-            Timber.d("==================")
-            Timber.d("$completeFirst $completeSecond")
-        }
-    }
-
-    private suspend fun completeFirst(): String {
-        delay(2000)
-        Timber.d("completeFirst")
-        return "completeFirst"
-    }
-
-    private suspend fun completeSecond(): String {
-        delay(1000)
-        Timber.d("completeSecond")
-        return "completeSecond"
     }
 }
