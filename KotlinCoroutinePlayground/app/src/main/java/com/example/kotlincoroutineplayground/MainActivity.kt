0866586513e9fed9fca95857744b3d8d106c371e
chiiia12 <chiakiyokoo@gmail.com>
2019-06-21 18:08:24 +0900

    challenge1

--- KotlinCoroutinePlayground/app/src/main/java/com/example/kotlincoroutineplayground/MainActivity.kt
+++ KotlinCoroutinePlayground/app/src/main/java/com/example/kotlincoroutineplayground/MainActivity.kt
@@ -11,5 +11,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.plant(Timber.DebugTree())
+        Timber.d("coroutine start")
+        uiScope.launch {
+            val completeFirst = completeFirst()
+            val completeSecond = completeSecond()
+            Timber.d("==================")
+            Timber.d("$completeFirst $completeSecond")
+        }
+    }
+
+    private suspend fun completeFirst(): String {
+        delay(2000)
+        Timber.d("completeFirst")
+        return "completeFirst"
+    }
+
+    private suspend fun completeSecond(): String {
+        delay(1000)
+        Timber.d("completeSecond")
+        return "completeSecond"
     }
 }
